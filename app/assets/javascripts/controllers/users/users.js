ec84f0be1a302c94b8e363f3451941b01df4e4bf
Santosh <nyros@nyros.(none)>
2014-07-09 16:26:36 +0530

    User's Search Added

--- app/assets/javascripts/controllers/users/users.js
+++ app/assets/javascripts/controllers/users/users.js
@@ -1,3 +1,15 @@
 App.UsersController = Ember.ArrayController.extend({
-  sortProperties: ['firstName', 'lastName']
+  sortProperties: ['firstName', 'lastName'],
+
+  users: function() {
+    return this.get('search') ? this.get('searchedUsers') : this
+  }.property('search', 'searchedUsers'),
+
+  searchedUsers: function() {
+    var search = this.get('search').toLowerCase()
+    return this.filter(function(user) {
+      return user.get('fullName').toLowerCase().indexOf(search) != -1
+    })
+  }.property('search', 'this.@each.fullName')
+
 })
